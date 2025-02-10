87bcc580e6d2c284714eaa4b13fa965a60c80125
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2025-02-10 19:22:50 +0100

    Feat: Get a book endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,8 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+@router.get("/{book_id}", status_code=status.HTTP_404_NOT_FOUND)
+async def get_book(book_id: int)->Book:
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, 
+                        content=db.get_book(book_id).model_dump())
