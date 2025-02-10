dd470e97e6a65c39439f566afa0e85115dd60e0f
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2025-02-10 19:50:36 +0100

    Feat: get book endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -61,7 +61,11 @@ async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
-@router.get("/{book_id}", status_code=status.HTTP_404_NOT_FOUND)
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int)->Book:
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, 
+    book = db.get_book(book_id)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, 
                         content=db.get_book(book_id).model_dump())
